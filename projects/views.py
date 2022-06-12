837d3d5063db182435790df54666c602642c7b18
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-12 15:26:41 +0300

    ft:create function that displays all projects

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from .models import Project
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import Profile,Project
+# from .forms import NewProjectForm,ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def home(request):
     projects=Project.objects.all()
     return render(request,'projects/home.html',{'projects':projects})
+
+@login_required(login_url='/accounts/login/') 
+def rate_project(request,project_id):
+    project=Project.objects.get(id=project_id)
+    return render(request,"awwards/project.html",{"project":project})
