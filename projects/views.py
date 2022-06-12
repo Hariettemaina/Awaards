fb6b71ac077005eed420ecfbedc3f408b6047733
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-12 15:36:38 +0300

    ft:function for user to register

--- projects/views.py
+++ projects/views.py
@@ -17,3 +17,18 @@ def home(request):
 def rate_project(request,project_id):
     project=Project.objects.get(id=project_id)
     return render(request,"projects/project.html",{"project":project})
+
+
+def register(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        user = User.objects.create_user(username=username,email=email,password=password1)
+        user.save()
+        profile=Profile.objects.create(user=user,email=user.email)
+        
+        return redirect('login')
+    else:
+        return render(request,'registration/registration_form.html')
