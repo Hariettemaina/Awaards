24109d5d38e056b6b637cd3c9631ef2a021498d0
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-12 22:35:59 +0300

    ft:define function for new projects

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,12 @@ from .models import Profile,Project
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+
 
 # Create your views here.
 def home(request):
@@ -32,3 +38,51 @@ def register(request):
         return redirect('login')
     else:
         return render(request,'registration/registration_form.html')
+    
+
+@login_required(login_url='/accounts/login')
+def search_project(request):
+    if 'project' in request.GET and request.GET['project']:
+        search_term=request.GET.get('project')
+        searched_projects=Project.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request,'projects/search.html',{"message":message, "projects":searched_projects, "project":search_term})
+
+    else:
+        message = "Please enter search name"
+
+    return render(request, 'projects/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')     
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('home')
+        
+    else:
+        form = NewProjectForm()
+    return render(request, 'awwards/new_project.html', {"form":form, "current_user":current_user})
+    
+@login_required(login_url='/accounts/login/')   
+def api_page(request):
+    return render(request,'awwards/api_page.html')
+
+
+class ProfileList(APIView):
+    def get(self, request, fromat=None):
+        all_profiles =Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+
+class ProjectList(APIView):
+    def get(self, request, fromat=None):
+        all_projects =Project.objects.all()
+        serializers =ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)          
