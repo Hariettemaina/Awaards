c2ef29b1a543a6780031f9e3eef901477f2acbdb
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-13 01:16:19 +0300

    styling

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import views 
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('projects.urls'))
+    path('',include('projects.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view, {"next_page": '/'}),
 ]
