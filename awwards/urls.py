dbd11cc291ca160002ba86c19491db2934ab86f6
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-13 11:38:03 +0300

    FT:API

--- awwards/urls.py
+++ awwards/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import logout 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view, {"next_page": '/'}),
+    path('logout/', views.LogoutView.as_view, {"next_page": '/'}),
 ]
